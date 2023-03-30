addc5de143ce63228c1f9d647751c11b015f8d09
huans <huang.shengbing@mayo.edu>
2023-03-30 11:08:35 -0500

    pipeline

--- src/trainer_image_vertex/task.py
+++ src/trainer_image_vertex/task.py
@@ -1,9 +1,19 @@
 import argparse
 import json
 import os
+import sys
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
 
-from parentdirectory import model
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
 
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+from parent import model
 import tensorflow as tf
 
 if __name__ == "__main__":
